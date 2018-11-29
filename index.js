1f4836eee90680b564898194eb6e843c3276e940
dhdc1 <dhdctraining@gmail.com>
2018-11-29 10:25:04 +0700

    tehnn

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var cors = require("cors");
 
-var path = require('path');
+var path = require("path");
 
 app.use(cors());
 
 //app.use(express.static(path.join(__dirname, 'web')));
 
-app.get("/web", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/web/index.html");
 });
 
 app.get("/test", function(req, res) {
   res.sendFile(__dirname + "/web/test.html");
 });
+
+var client = require("socket.io-client")("http://localhost:19009");
+
+app.get("/scn", async (req, res) => {
+  await client.emit("scn", "scn");
+  res.send("scn");
+});
+
+app.get("/doc", async (req, res) => {
+  await client.emit("doc", "doc");
+  res.send("doc");
+});
+
+app.get("/lab", async (req, res) => {
+  await client.emit("lab", "lab");
+  res.send("lab");
+});
+
+app.get("/xry", async (req, res) => {
+  await client.emit("xry", "xry");
+  res.send("xry");
+});
+
+app.get("/drg", async (req, res) => {
+  await client.emit("drg", "drg");
+  res.send("deg");
+});
+
+app.get("/fin", async (req, res) => {
+  await client.emit("fin", "fin");
+  res.send("fin");
+});
+
+
 
 io.on("connection", function(socket) {
   // for test
@@ -32,32 +66,29 @@ io.on("connection", function(socket) {
     io.emit("scn", "scn msg");
   });
   socket.on("doc", function() {
-    io.emit("doc", "scn msg");
+    io.emit("doc", "doc msg");
   });
-
 
   //update จอ wait-finance
   socket.on("fin", function() {
-    io.emit("fin", "fin");
+    io.emit("fin", "fin msg");
   });
 
   //update จอ wait-drug
   socket.on("drg", function() {
-    io.emit("drg", "drg");
+    io.emit("drg", "drg msg");
   });
 
   //update จอ wait-lab
   socket.on("lab", function() {
-    io.emit("lab", "lab");
+    io.emit("lab", "lab msg");
   });
 
   //update จอ wait-aray
   socket.on("xry", function() {
-    io.emit("xry", "xry");
-    
+    io.emit("xry", "xry msg");
   });
 });
-
 
 var port = process.env.PORT || 19009;
 http.listen(port, function() {
