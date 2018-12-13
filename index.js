db2f7671dbfdfec40d960a0ef76329e9633739fa
dhdc1 <dhdctraining@gmail.com>
2018-12-13 09:09:14 +0700

    tehnn

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ io.on("connection", function(socket) {
   socket.on("blink", function(q) {
     let date = new Date();
     let d = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-    console.log(`[${d}] =>`, q);
+    console.log(`[${d}]`, q);
     io.emit("blink", q);
   });
 });
