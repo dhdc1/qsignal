41434b15f049cd65e7b147d446dd39eaa2524c4c
dhdc1 <dhdctraining@gmail.com>
2018-12-16 19:56:57 +0700

    tehnn

--- index.js
+++ index.js
@@ -25,123 +25,120 @@ var client = require("socket.io-client")(`http://localhost:${port}`);
 
 app.get("/scn/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("scn", "scn");
-  await client.emit("blink", q);
+  await client.emit("scn", q);
   res.send("scn");
 });
 
 app.get("/doc/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("doc", "doc");
-  await client.emit("blink", q);
+  await client.emit("doc", q);
   res.send("doc");
 });
 
 app.get("/lab/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("lab", "lab");
-  await client.emit("blink", q);
+  await client.emit("lab", q);;
   res.send("lab");
 });
 
 app.get("/xry/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("xry", "xry");
-  await client.emit("blink", q);
+  await client.emit("xry", q);
   res.send("xry");
 });
 
 app.get("/drg/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("drg", "drg");
-  await client.emit("blink", q);
+  await client.emit("drg", q);
   res.send("deg");
 });
 
 app.get("/fin/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("fin", "fin");
-  await client.emit("blink", q);
+  await client.emit("fin", q);
   res.send("fin");
 });
 
 app.get("/err/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("err", "err");
-  await client.emit("blink", q);
+  await client.emit("err", q);
   res.send("err");
 });
 
 app.get("/fnt/:q", async (req, res) => {
   let q = req.params.q;
-  await client.emit("fnt", "fnt");
-  await client.emit("blink", q);
+  await client.emit("fnt", q);
   res.send("fnt");
 });
 
-// {ฝั่ง server ส กระจายสัญญาณ}
+
+// {ฝั่ง server กระจายสัญญาณ}
 io.on("connection", function(socket) {
-  // for test
+  
   let ip = socket.handshake.address;
-  console.log(ip, " connected...");
-  socket.on("chat", function(msg) {
-    io.emit("chat", ip + " >> " + msg);
-  });
-  // end for test
+  console.log(ip, "connected");  
+ 
 
   // update จอ wait-screen
-  socket.on("scn", async function() {
+  socket.on("scn", async function(q) {
+    console.log(`scn from ${ip}` ,q);
     await io.emit("scn", "scn msg");
+    await io.emit("blink", q);
   });
-  socket.on("doc", async function() {
+  socket.on("doc", async function(q) {
+    console.log(`doc from ${ip}` ,q);
     await io.emit("doc", "doc msg");
+    await io.emit("blink", q);
   });
 
   //update จอ wait-finance
-  socket.on("fin", async function() {
+  socket.on("fin", async function(q) {
+    console.log(`fin from ${ip}` ,q);
     await io.emit("fin", "fin msg");
+    await io.emit("blink", q);
   });
 
   //update จอ wait-drug
-  socket.on("drg", async function() {
+  socket.on("drg", async function(q) {
+    console.log(`drg from ${ip}` ,q);
     await io.emit("drg", "drg msg");
+    await io.emit("blink", q);
   });
 
   //update จอ wait-lab
-  socket.on("lab", async function() {
+  socket.on("lab", async function(q) {
+    console.log(`lab from ${ip}` ,q);
     await io.emit("lab", "lab msg");
+    await io.emit("blink", q);
   });
 
   //update จอ wait-aray
-  socket.on("xry", async function() {
+  socket.on("xry", async function(q) {
+    console.log(`xry from ${ip}` ,q);
     await io.emit("xry", "xry msg");
+    await io.emit("blink", q);
   });
 
   //update จอ wait-err
-  socket.on("err", async function() {
+  socket.on("err", async function(q) {
+    console.log(`err from ${ip}` ,q);
     await io.emit("err", "err msg");
+    await io.emit("blink", q);
   });
 
   //update จอ wait-front-nurse
-  socket.on("fnt", async function() {
+  socket.on("fnt", async function(q) {
+    console.log(`fnt from ${ip}` ,q);
     await io.emit("fnt", "fnt msg");
+    await io.emit("blink", q);
   });
 
-  //ส่งสัญญาณกระพริบระบุ Queue
-  socket.on("blink", async function(q) {
-    //let date = new Date();
-    //let d = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-    //console.log(`[${d}] blink => `, q);
-    //console.log(q)
-    if (q != "s99999") {
-      await io.emit("blink", q);
-    }
-  });
+ 
 });
 
 // timmer ส่งสลับ page
 let page = 1;
-let timmerPage = setInterval(() => {
+/*let timmerPage = setInterval(() => {
   if (page == 1) {
     page = 2;
   } else {
@@ -149,7 +146,7 @@ let timmerPage = setInterval(() => {
   }
   console.log("page = ", page);
   io.emit("page", page);
-}, 1000 * 10);
+}, 1000 * 10);*/
 
 // จบ timmer ส่ง สลับ page
 
